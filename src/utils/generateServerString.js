fc94481f9e76e42858ed2930b282048000f11964
Dorian Niemiec <dorian.niemiec@svrjs.org>
2025-01-08 15:26:47 +0100

    perf: generate the server string before calling the generateServerString function

--- src/utils/generateServerString.js
+++ src/utils/generateServerString.js
@@ -1,20 +1,19 @@
 const svrjsInfo = require("../../svrjs.json");
 const { version, name } = svrjsInfo;
 const getOS = require("./getOS.js");
+const serverStringWithVersion = `${name.replace(/ /g, "-")}/${version} (${getOS()}; ${
+  process.isBun
+    ? "Bun/v" + process.versions.bun + "; like Node.js/" + process.version
+    : process.versions && process.versions.deno
+      ? "Deno/v" + process.versions.deno + "; like Node.js/" + process.version
+      : "Node.js/" + process.version
+})`;
+const serverStringWithoutVersion = name.replace(/ /g, "-");
 
 function generateServerString(exposeServerVersion) {
   return exposeServerVersion
-    ? `${name.replace(/ /g, "-")}/${version} (${getOS()}; ${
-        process.isBun
-          ? "Bun/v" + process.versions.bun + "; like Node.js/" + process.version
-          : process.versions && process.versions.deno
-            ? "Deno/v" +
-              process.versions.deno +
-              "; like Node.js/" +
-              process.version
-            : "Node.js/" + process.version
-      })`
-    : name.replace(/ /g, "-");
+    ? serverStringWithVersion
+    : serverStringWithoutVersion;
 }
 
 module.exports = generateServerString;
