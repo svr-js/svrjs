c95a618c795f6815788ccdbe14283a1d6182f565
Dorian Niemiec <dorian.niemiec@svrjs.org>
2025-01-23 15:09:55 +0100

    Revert "perf: generate the server string before calling the generateServerString function"
    This reverts commit fc94481f9e76e42858ed2930b282048000f11964.

--- src/utils/generateServerStringCore.js
+++ src/utils/generateServerStringCore.js
@@ -3,19 +3,20 @@ const { name } = svrjsCoreInfo;
 const svrjsInfo = require("../../svrjs.json");
 const { version } = svrjsInfo;
 const getOS = require("./getOS.js");
-const serverStringWithVersion = `${name.replace(/ /g, "-")}/${version} (${getOS()}; ${
-  process.isBun
-    ? "Bun/v" + process.versions.bun + "; like Node.js/" + process.version
-    : process.versions && process.versions.deno
-      ? "Deno/v" + process.versions.deno + "; like Node.js/" + process.version
-      : "Node.js/" + process.version
-})`;
-const serverStringWithoutVersion = name.replace(/ /g, "-");
 
 function generateServerString(exposeServerVersion) {
   return exposeServerVersion
-    ? serverStringWithVersion
-    : serverStringWithoutVersion;
+    ? `${name.replace(/ /g, "-")}/${version} (${getOS()}; ${
+        process.isBun
+          ? "Bun/v" + process.versions.bun + "; like Node.js/" + process.version
+          : process.versions && process.versions.deno
+            ? "Deno/v" +
+              process.versions.deno +
+              "; like Node.js/" +
+              process.version
+            : "Node.js/" + process.version
+      })`
+    : name.replace(/ /g, "-");
 }
 
 module.exports = generateServerString;
